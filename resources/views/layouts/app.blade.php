146573de71aa86168b0e3cda409fdfbbc0074109
syazwan <28nursyazwan@gmail.com>
2025-04-07 16:33:22 +0800

    tambah design untuk page index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Default Title')</title> <!-- Set Default Title -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"> <!-- Link to CSS file -->
 </head>
 <body>
